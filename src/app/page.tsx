95a4d2c9848ab339061f60eca1bd3abe090dd03a
naruse-k <xnaruse.kazuki@contract.isid.co.jp>
2024-06-04 02:32:10 +0900

    feat:テキスト入力による住所の動的検索

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,56 @@
 "use client";
 
-import { on } from "events";
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [address, setAddress] = useState<any>();
-  const [inputText, setInputText] = useState<string>("");
-  const fetchAddress = async () => {
-    setAddress(
-      await fetch(
-        `https://zipcloud.ibsnet.co.jp/api/search?zipcode=1010041`
-      ).then((res) => res.json())
-    );
+  const [address, setAddress] = useState<any[]>([]);
+  const [inputText, setInputText] = useState("");
+
+  const fetchAddress = async (zipCode: string = "1010041") => {
+    const response = await fetch(
+      `https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipCode}`
+    ).then((res) => res.json());
+    if (response?.results[0]) {
+      setAddress([...address, response.results[0]]);
+    }
   };
 
   useEffect(() => {
     fetchAddress();
   }, []);
-  console.log(address?.results?.address1);
+
   const onClickAddressSearch = async () => {
-    // const inputText = document.getElementById("textInput") as HTMLInputElement;
-    setAddress(
-      await fetch(
-        `https://zipcloud.ibsnet.co.jp/api/search?zipcode=${inputText}`
-      ).then((res) => res.json())
-    );
+    fetchAddress(inputText);
   };
-  const onChangeInputText = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputText(e.target.value);
   };
 
-  // const testData: any = await fetch(
-  //   `https://openapi.city.shizuoka.jp/opendataapi/servicepoint/roadRegulation`
-  // ).then((res) => res.json());
-  // // console.log(testData);
-  // const onClickBtn = () => {
-  //   console.log(testData.Data.features[0].properties.regulation_type);
-  // };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        {address?.results?.address1}
-        {address?.results?.address2}
-        {address?.results?.address3}
+    <main className="flex min-h-screen flex-col items-center p-24 bg-gradient-to-r from-indigo-400">
+      {address.map((e: any, i) => (
+        <div key={i} className="flex space-x-4 my-8 border-b-2 w-2/3">
+          <div className="flex-1">{e.zipcode}</div>
+          <div className="flex-1">{e.address1}</div>
+          <div className="flex-1">{e.address2}</div>
+          <div className="flex-1">{e.address3}</div>
+        </div>
+      ))}
+      <div className="flex space-x-4">
+        <input
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder="郵便番号(ハイフンなし)"
+          className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 flex-1"
+        />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex-1"
+          onClick={onClickAddressSearch}
+        >
+          住所検索
+        </button>
       </div>
-      <input
-        type="text"
-        id="textInput"
-        name={inputText}
-        value={inputText}
-        placeholder="ここにテキストを入力してください"
-        onChange={(e) => {
-          onChangeInputText(e);
-        }}
-      ></input>
-      <button onClick={onClickAddressSearch}>住所検索</button>
-      {/* <div>{testData.Data.features[0].properties.regulation_type}</div>
-      <button onClick={onClickBtn}>交通情報取得</button> */}
     </main>
   );
 }
